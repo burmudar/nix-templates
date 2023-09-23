508f4348a9a99d7676e836a0962328d71000c406
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2023-09-24 00:09:39 +0200

    add template for basic dev shell

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A Nix flake for templates";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+
+      templates = {
+        basic = {
+          path = "./basic";
+          description = "very basic nix flake template";
+        };
+      };
+    };
+}
